0c9a6faddb471b8f0f3c391a53db9a030f657d6a
Dora-14 <91243919+Dora-14@users.noreply.github.com>
2022-05-05 07:43:14 +0300

    Add files via upload

---
+++ erettsegi21/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+    int divPrim (int n){
+      int oszto=2;
+      int S=0;
+      while(n>1){
+        int szamlalo=0;
+            while(n%oszto==0){
+                n=n/oszto;
+                szamlalo++;
+            }
+            if(szamlalo%2>0){
+                S=S+oszto;
+            }
+            oszto++;
+        }
+        return S;
+
+      }
+
+int main(){
+    int s=divPrim(16);
+    cout << s << endl;
+
+    return 0;
+}
